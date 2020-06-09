d3a3215d903e5bc65143b4964286c5841393a685
pablo-eg <pabloe.garcia@alumnos.frm.utn.edu.ar>
2020-06-09 18:56:07 -0300

    dBm to Voltage calculator

---
+++ dBm2v.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
+
+# Para los calculos
+import numpy as np
+
+import  math
+
+import scipy                  # http://scipy.org/
+from scipy import constants
+
+def dBmtoV(dBm):
+
+    v = 10**(dBm/20)
+
+    print('\nV = {:.2f} mV\n\n'.format(v))
+
+while 1:
+    dBm = float(input("Enter dBm: "))
+    dBmtoV(dBm)
+    pass
